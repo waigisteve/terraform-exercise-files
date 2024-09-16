5260d4140181aadfa9f0ca9595a71c813c288364
waigisteve <120468516+waigisteve@users.noreply.github.com>
2024-09-16 22:54:29 +0300

    create new dev vpc

--- main.tf
+++ main.tf
@@ -18,11 +18,28 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
@@ -34,11 +51,10 @@ module "blog_sg" {
   version = "5.2.0"
   name = "blog"
 
-  vpc_id = data.aws_vpc.default.id
-
+  vpc_id = module.blog_vpc.vpc_id
+  name = "blog"
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
