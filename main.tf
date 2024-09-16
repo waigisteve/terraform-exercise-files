2b4895af285bb4860d4e1fc2b83c253d6602d002
waigisteve <120468516+waigisteve@users.noreply.github.com>
2024-09-16 23:21:27 +0300

    create new dev vpc

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region = "us-west-2" # Ensure the correct region
+}
+
+# Fetch the most recent Bitnami Tomcat AMI
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -14,18 +19,20 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+# Get default VPC (optional if needed)
 data "aws_vpc" "default" {
   default = true
 }
 
+# VPC Module for the blog
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
-
+  
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]  # Change AZs to match your region
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]  # Ensure CIDRs match the VPC
 
   tags = {
     Terraform   = "true"
@@ -33,24 +40,26 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id              = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
-}
-
+# Security Group Module for the blog instance
 module "blog_sg" {
   source              = "terraform-aws-modules/security-group/aws"
   version             = "5.2.0"
   name                = "blog"
-  vpc_id              = module.blog_vpc.vpc_id
+  vpc_id              = module.blog_vpc.vpc_id  # Reference the correct VPC ID
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]  # Open to the world for HTTP/HTTPS traffic
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
+}
+
+# AWS EC2 Instance for the blog
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  subnet_id              = element(module.blog_vpc.public_subnets, 0)  # Reference the first public subnet
+
+  tags = {
+    Name = "Learning Terraform"
+  }
 }
