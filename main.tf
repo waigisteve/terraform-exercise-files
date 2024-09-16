86ab2b84a9af7595ed43f7003141358a6555bba8
waigisteve <120468516+waigisteve@users.noreply.github.com>
2024-09-16 22:08:26 +0300

    use modules for security

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
-  Name = "blog_new"
+  name = "blog_new"
 
   vpc_id = data.aws_vpc.default.id
 
