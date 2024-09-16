807cb8a05a7d6d6a0c0de4e7dd5a5d6bcc17baf0
waigisteve <120468516+waigisteve@users.noreply.github.com>
2024-09-16 23:26:45 +0300

    create new dev vpc

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 provider "aws" {
-  region = "us-west-2" # Ensure the correct region
+  region = var.aws_region  # Assuming `aws_region` is passed in as a variable
 }
 
-# Fetch the most recent Bitnami Tomcat AMI
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -19,20 +18,18 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-# Get default VPC (optional if needed)
 data "aws_vpc" "default" {
   default = true
 }
 
-# VPC Module for the blog
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  
+
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]  # Change AZs to match your region
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]  # Ensure CIDRs match the VPC
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
     Terraform   = "true"
@@ -40,26 +37,24 @@ module "blog_vpc" {
   }
 }
 
-# Security Group Module for the blog instance
-module "blog_sg" {
-  source              = "terraform-aws-modules/security-group/aws"
-  version             = "5.2.0"
-  name                = "blog"
-  vpc_id              = module.blog_vpc.vpc_id  # Reference the correct VPC ID
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]  # Open to the world for HTTP/HTTPS traffic
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
-}
-
-# AWS EC2 Instance for the blog
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id              = element(module.blog_vpc.public_subnets, 0)  # Reference the first public subnet
+  subnet_id              = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
   }
+}
+
+module "blog_sg" {
+  source              = "terraform-aws-modules/security-group/aws"
+  version             = "5.2.0"
+  name                = "blog"
+  vpc_id              = module.blog_vpc.vpc_id
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
