bc202b4c6706160fab74e42ff68c70beed44d139
waigisteve <120468516+waigisteve@users.noreply.github.com>
2024-09-16 20:54:13 +0300

    create security group

--- main.tf
+++ main.tf
@@ -22,10 +22,10 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+vpc_security_group_ids = [aws_security_group.blog.id]
 
-  tags = {
-    Name = "HelloWorld"
+tags = {
+    Name = "Learning Terraform"
   }
 }
 
@@ -36,7 +36,7 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource :aws_security_group_rule"  :blog_http_in" {
+resource "aws_security_group_rule"  "blog_http_in" {
   type        =  "ingress"
   from_port   = 80
   to_port     = 80
@@ -46,7 +46,7 @@ resource :aws_security_group_rule"  :blog_http_in" {
   security _group_id = aws_security_group.blog.id
 }
 
-resource :aws_security_group_rule"  :blog_http_in" {
+resource :aws_security_group_rule"  "blog_https_in" {
   type        =  "ingress"
   from_port   = 443
   to_port     = 443
@@ -56,7 +56,7 @@ resource :aws_security_group_rule"  :blog_http_in" {
   security _group_id = aws_security_group.blog.id
 }
 
-resource :aws_security_group_rule"  :blog_everything_out" {
+resource :aws_security_group_rule"  "blog_everything_out" {
   type        =  "egress"
   from_port   = 0
   to_port     = 0
