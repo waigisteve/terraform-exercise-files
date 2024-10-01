485fdaa71a1996a469c5278d6211b0e80a88ea63
waigisteve <120468516+waigisteve@users.noreply.github.com>
2024-10-01 16:43:22 +0300

    load balancer

--- main.tf
+++ main.tf
@@ -14,7 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-
 data "aws_vpc" "default" {
   default = true
 }
@@ -26,12 +25,10 @@ module "_blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
@@ -62,22 +59,31 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "blog_sg" {
+  source              = "terraform-aws-modules/security-group/aws"
+  version             = "5.2.0"
+  name                = "blog"
+  vpc_id              = module.blog_vpc.vpc_id
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
+}
+
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name               = "blog-alb"
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = module.blog_sg.security_group_id
-}
+  name            = "blog-alb"
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = [module.blog_sg.security_group_id]
 
   http_tcp_listeners = [
-   {
-      port     = 80
-      protocol = "HTTP"
-      target_group_index = 0
-      
-      }
+    {
+      port                = 80
+      protocol            = "HTTP"
+      target_group_index  = 0
+    }
   ]
 
   target_groups = {
@@ -94,16 +100,4 @@ module "alb" {
     Environment = "Dev"
     Project     = "Example"
   }
-
-
-
-module "blog_sg" {
-  source              = "terraform-aws-modules/security-group/aws"
-  version             = "5.2.0"
-  name                = "blog"
-  vpc_id              = module.blog_vpc.vpc_id
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
