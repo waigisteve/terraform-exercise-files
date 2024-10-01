0eef4e13f5fb777a9c7e040dbaa2d118fb62e4cf
waigisteve <120468516+waigisteve@users.noreply.github.com>
2024-10-01 17:02:02 +0300

    load balancer

--- main.tf
+++ main.tf
@@ -1,112 +1,38 @@
-# providers.tf
-provider "aws" {
-  region = "us-west-2"
-}
-
-# main.tf
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-module "_blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
-
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
-
+  
   name = "dev"
   cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  
+  azs = ["us-west-2a", "us-west-2b", "us-west-2c"]
 
   tags = {
-    Terraform   = "true"
     Environment = "dev"
   }
-}
-
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id              = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
-}
-
-module "blog_sg" {
-  source              = "terraform-aws-modules/security-group/aws"
-  version             = "5.2.0"
-  name                = "blog"
-  vpc_id              = module.blog_vpc.vpc_id
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
+  
+  providers = {
+    aws = aws.secondary  # Use aliased provider if needed
+  }
 
-  name               = "blog-alb"
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = module.blog_sg.security_group_id
-
-  # Configure listeners
+  name            = "blog-alb"
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+  
   listener {
     port     = 80
     protocol = "HTTP"
-
+  
     default_action {
       type = "forward"
       target_group_arn = module.alb_target_group.target_group_arn
     }
   }
-
-  # Configure target groups
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
-    }
-  }
-
+  
   tags = {
     Environment = "Dev"
     Project     = "Example"
@@ -115,7 +41,7 @@ module "alb" {
 
 module "alb_target_group" {
   source = "terraform-aws-modules/alb/aws//modules/target-group"
-
+  
   name_prefix = "blog"
   port        = 80
   protocol    = "HTTP"
