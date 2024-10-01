d5fdf15d19a664b3f746632e2fbf265e639e29e2
waigisteve <120468516+waigisteve@users.noreply.github.com>
2024-10-01 15:36:40 +0300

    load balancer

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "us-west-2"  # Specify your AWS region
+}
+
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -21,41 +25,15 @@ data "aws_vpc" "default" {
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name            = "dev"
-  cidr            = "10.0.0.0/16"
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
     Terraform   = "true"
     Environment = "dev"
-  }
-}
-
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id              = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
-}
-
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
-
-  name                = "blog-alb"
-  load_balancer_type  = "application"
-  vpc_id              = module.blog_vpc.vpc_id
-  subnets             = module.blog_vpc.public_subnets
-  security_groups     = [module.blog_sg.security_group_id]
-
-  tags = {
-    Environment = "Dev"
-    Project     = "Example"
   }
 }
 
@@ -70,8 +48,60 @@ module "blog_sg" {
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  subnet_id              = module.blog_vpc.public_subnets[0]
+
+  tags = {
+    Name = "Learning Terraform"
+  }
+}
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name    = "blog-alb"
+  load_balancer_type = "application"
+
+  vpc_id           = module.blog_vpc.vpc_id
+  subnets          = module.blog_vpc.public_subnets
+  security_groups  = [module.blog_sg.security_group_id]
+
+  # Ingress and Egress Rules are now defined directly in the security group module
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
+
+  tags = {
+    Environment = "Dev"
+    Project     = "Example"
+  }
+}
+
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = module.alb.lb_arn  # Correct reference to the ALB ARN
+  load_balancer_arn = module.alb.load_balancer_arn  # Correct reference to the ALB ARN
   port              = 80
   protocol          = "HTTP"
 
@@ -87,31 +117,12 @@ resource "aws_lb_listener" "http" {
 }
 
 resource "aws_lb_listener" "https" {
-  load_balancer_arn = module.alb.lb_arn  # Correct reference to the ALB ARN
+  load_balancer_arn = module.alb.load_balancer_arn  # Correct reference to the ALB ARN
   port              = 443
   protocol          = "HTTPS"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.blog.arn
+    target_group_arn = module.alb.target_groups.ex-instance.arn  # Ensure correct reference to the target group ARN
   }
-}
-
-resource "aws_lb_target_group" "blog" {
-  name        = "blog-target-group"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = module.blog_vpc.vpc_id
-  target_type = "instance"
-
-  health_check {
-    path = "/"
-    port = "80"
-  }
-}
-
-resource "aws_lb_target_group_attachment" "blog" {
-  target_group_arn = aws_lb_target_group.blog.arn
-  target_id        = aws_instance.blog.id
-  port             = 80
 }
