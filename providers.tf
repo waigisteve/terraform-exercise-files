caf5b927288506184c570be89bbff38413335b7c
waigisteve <120468516+waigisteve@users.noreply.github.com>
2024-10-01 17:01:17 +0300

    pr

--- providers.tf
+++ providers.tf
@@ -2,10 +2,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
+}
+
+provider "aws" {
+  alias  = "secondary"
+  region = "us-east-1"
 }
