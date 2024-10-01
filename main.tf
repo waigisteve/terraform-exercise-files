851a98e6b058dbc4d615bf6793f764b452c805a4
waigisteve <120468516+waigisteve@users.noreply.github.com>
2024-10-01 16:31:22 +0300

    load balancer

--- main.tf
+++ main.tf
@@ -71,29 +71,7 @@ module "alb" {
   security_groups    = module.blog_sg.security_group_id
 }
 
-  # Security Group
-  security_group_ingress_rules = {
-    all_http = {
-      from_port   = 80
-      to_port     = 80
-      ip_protocol = "tcp"
-      description = "HTTP web traffic"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-    all_https = {
-      from_port   = 443
-      to_port     = 443
-      ip_protocol = "tcp"
-      description = "HTTPS web traffic"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-  }
-  security_group_egress_rules = {
-    all = {
-      ip_protocol = "-1"
-      cidr_ipv4   = "10.0.0.0/16"
-    }
-  }
+
 
 
   listeners = {
